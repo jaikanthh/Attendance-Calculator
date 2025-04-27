f7e7b5d7e8cba7896f77fca28cfe7f9071bed0ba
Jaikanth <2320090050@klh.edu.in>
2025-04-27 11:50:10 +0530

    App update

--- KLUAttendanceMobile/index.js
+++
@@ -1,5 +0,0 @@
-import { registerRootComponent } from 'expo';
-import App from './App';
-
-// Register the app component as the main component
-registerRootComponent(App); 
--- src/types/navigation.ts
+++
@@ -1,6 +0,0 @@
-export type RootStackParamList = {
-  Welcome: undefined;
-  Home: undefined;
-  SimpleCalculator: undefined;
-  LTPSCalculator: undefined;
-}; 
